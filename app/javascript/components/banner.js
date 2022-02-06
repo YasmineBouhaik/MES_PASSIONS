8325a398882bf6be666359025cc1701ae8d25970
YasmineBouhaik <yasmine.bouhaik@gmail.com>
2022-02-06 16:28:26 +0100

    details

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -5,7 +5,7 @@ const loadDynamicBannerText = () => {
   const banner = document.querySelector('.banner');
   if (banner) {
     new Typed('#banner-typed-text', {
-      strings: ["Sport 🚴‍♀️, Musique 🥁, Voyages 🌎"],
+      strings: ["🚴‍♀️ 🥁 🌎"],
       typeSpeed: 60,
       loop: true
     });
