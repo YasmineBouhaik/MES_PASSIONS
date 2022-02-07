9033ad98893da0e80f2e8f0734b18a45f1e7908f
YasmineBouhaik <yasmine.bouhaik@gmail.com>
2022-02-07 12:25:37 +0100

    details margin

--- app/controllers/trip_controller.rb
+++ app/controllers/trip_controller.rb
@@ -2,10 +2,10 @@ class TripController < ApplicationController
   def index
     @trip = Trip.all
     @markers = @trip.geocoded.map do |trip|
-    {
-      lat: flat.latitude,
-      lng: flat.longitude
-    }
+      {
+        lat: flat.latitude,
+        lng: flat.longitude
+      }
     end
   end
 end
